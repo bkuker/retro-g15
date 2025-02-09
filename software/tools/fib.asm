1beb8b4707278d17fba5f3e49e0e1f3adcb557d2
bkuker <bkuker@billkuker.com>
2025-02-09 10:51:36 -0500

    Format one output per line, no leading zeros

--- software/tools/fib.asm
+++ software/tools/fib.asm
@@ -3,7 +3,7 @@
 # Copy loaded program from 19 -> 0 and begin execution at 0:04
 .00 . u.01.02.0.19.00     Line 19 to Line 0 - Test not set
 .01 . u.02.02.0.19.00     Line 19 to Line 0 - Test set
-.02 .  .04.04.0.21.31     Execute Line 0 at T4
+.02 .  .04.42.0.21.31     Execute Line 0 GOTO 0:42 to set formatting
 
 # Print value A to typewriter
 .04 .  .30.05.1.00.28     0.30 -> ARc   AR = A
@@ -28,3 +28,17 @@
 .30 1                     A
 .31 1                     B
 .32 0                     C
+
+#Formatting
+# Default formatting is 03:03 = -8w00000 03:02 = 1000000, but
+# These format codes will do no leading zero one value per line
+.40 0000000              F2 Format code for 03:02
+.41 0000044              F3 Format code for 03:03
+
+# 3:02 = 1000000
+.42 .  .40.43.1.00.28     AR = F2
+.43 .  .02.44.1.28.03     03:02 = AR
+
+# 03:03 = -8w00000
+.44 .  .41.45.1.00.28     AR = F3
+.45 .  .03.04.1.28.03     03:02 = AR, GOTO 4
